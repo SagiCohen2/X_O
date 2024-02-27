14a272324b1a2292df59c91ce5f03e932ff7ec71
sagi cohen <sagicohennnn@gmail.com>
2024-02-27 22:03:45 +0200

    working on x_o game

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 <h3>I made this one just for fun and to practice :)<br>
 X_O Game.</h3><br>
-<h2>Have Fun!!</h2>
+<h2>Have Fun!!</h2><br/><hr/>
+<h4>Made with JS,HTML and CSS.</h4>
--- game.js
+++ game.js
@@ -1,2 +1,5 @@
 let playerTxt = document.getElementById("playerTxt")
 let restBtn = document.getElementById("restBtn")
+let boxes = Array.from(document.getElementsByClassName('box'))
+
+console.log(boxes)
