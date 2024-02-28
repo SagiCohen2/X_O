701d7b6df20e3016b58cd078483d15297e3577af
sagi cohen <sagicohennnn@gmail.com>
2024-02-28 15:44:40 +0200

    last changes

--- game.js
+++ game.js
@@ -6,6 +6,8 @@ let boxes = Array.from(document.getElementsByClassName('box'))
 
 // console.log(boxes)
 
+let winningIndicator = getComputedStyle(document.body).getPropertyValue('--winning')
+
 const O_Text = "O"
 const X_Text = "X"
 let currentPlayer = X_Text
@@ -24,10 +26,45 @@ function boxClicked(e) {
     if(!spaces[id]) {
     spaces[id] = currentPlayer
     e.target.innerText = currentPlayer
+
+    if (playerWon() !==false) {
+        playerTxt = `${currentPlayer} HAS WON!`
+        let winnig_blocks = playerWon()
+
+        // console.log(winnig_blocks)
+
+        winnig_blocks.map(box => boxes[box].style.backgroundColor = winningIndicator)
+        return
+
+    }
     
     currentPlayer = currentPlayer == X_Text ? O_Text : X_Text
     }
     // console.log(e.target)
+}
+
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+function playerWon() {
+    for (const condition of winningCombos) {
+        let [a,b,c] = condition
+
+        if (spaces[a] && spaces[a] == spaces[b] && spaces[a] == spaces[c]) {
+            return [a,b,c]
+        }
+    }
+
+    return false
+
 }
 
 // THE RESTART BUTTON
@@ -39,7 +76,10 @@ function restart() {
 
     boxes.forEach(box => {
         box.innerText = ''
+        box.style.backgroundColor = ''
     })
+
+    playerTxt = 'X_O Game :)'
 
     currentPlayer = X_Text
 }
